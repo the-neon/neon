1547de41884e459c5aa1e2658292a5e0bf5e1e27
stojce <stojce@me.com>
2020-08-24 09:57:53 +0200

    DEcorators

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,3 @@
-export * from "./decorators";
-export * from "./enums";
-export * from "./errors";
-export * from "./IAppContext";
-export * from "./IAuthenticatedUser";
+class Api { }
 
-export type integer = number;
-export type long = number;
-export type float = number;
-export type timestamp = Date;
+export default Api;
