55d33c3701a59716b8e8637f9849b6d4634ba009
stojce <stojce@me.com>
2020-09-01 12:14:36 +0200

    neon parser

--- bin/neon.js
+++ bin/neon.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 
 "use strict";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("../src/packages/cli/lib").run(process.argv.slice(2));
+require("../packages/gql/lib/generator");
