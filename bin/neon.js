db362b3478711fad2b03d0177f2ffd3151991cfa
stojce <stojce@me.com>
2020-09-01 10:11:28 +0200

    cli

--- bin/neon.js
+++ bin/neon.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
 "use strict";
-require("../packages/gql/lib/generator");
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("../src/packages/cli/lib").run(process.argv.slice(2));
