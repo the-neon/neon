d349098cbea8917015b5ff45fa0ada8385041b6d
Krste Bozhinoski <krste.bozinoski@alite-international.com>
2020-09-11 15:21:41 +0200

    mysql package

--- packages/mysql/src/index.ts
+++ packages/mysql/src/index.ts
@@ -1,9 +1,10 @@
 import { camelCase, snakeCase } from "change-case";
-import { QueryTypes, Transaction } from "sequelize";
+import { Transaction } from "sequelize";
 import { connection } from "./connection";
+import QueryTypes from "./QueryTypes";
 
 class MySqlDb {
-  private transaction?: Transaction = undefined;
+  private transaction?: Transaction;
 
   public async start(): Promise<void> {
     if (!this.transaction) {
@@ -11,17 +12,29 @@ class MySqlDb {
     }
   }
 
+  public async commit(): Promise<void> {
+    if (this.transaction) {
+      await this.transaction.commit();
+      this.transaction = undefined;
+    }
+  }
+
   async execute(
     sql: string,
-    params: any,
+    params?: Record<string, unknown>,
     queryType = QueryTypes.SELECT
-  ): Promise<any> {
+  ): Promise<Record<string, unknown> | Record<string, unknown>[] | null> {
     try {
       const rows = (await connection.query(sql, {
         replacements: params,
         type: queryType,
         transaction: this.transaction,
-      })) as any[];
+      })) as unknown[];
+
+      if (queryType === QueryTypes.SELECT) {
+        const results = rows.map((row) => this.castRow(row));
+        return results;
+      }
 
       if (queryType === QueryTypes.INSERT) {
         const [id] = rows;
@@ -30,44 +43,65 @@ class MySqlDb {
 
       if (queryType === QueryTypes.UPDATE) {
         const [, affectedRows] = rows;
-        if (affectedRows) {
-          return { params };
-        }
-        return null;
+        return affectedRows ? { params } : null;
       }
 
       if (queryType === QueryTypes.DELETE) {
-        if (!rows) {
-          return { params };
-        }
-        return null;
+        return rows ? null : { params };
       }
-
-      const results = rows.map((row) => {
-        const keys = Object.keys(row);
-        const model = {};
-        keys.forEach((key) => (model[camelCase(key)] = row[key]));
-        return model;
-      });
-
-      return results;
     } catch (e) {
       // eslint-disable-next-line
       console.log(JSON.stringify(e));
-      return null;
     }
+    return null;
   }
 
-  async getOne(sql, params): Promise<any> {
-    const results = await this.execute(sql, params);
-    return results && results[0];
+  async getOne(
+    sqlOrTable: string,
+    params?: Record<string, unknown>
+  ): Promise<Record<string, unknown>> {
+    let results;
+    if (sqlOrTable.toLowerCase().startsWith("select")) {
+      results = await this.execute(sqlOrTable, params);
+    } else {
+      let condition = "";
+      if (params) {
+        condition = `WHERE ${Object.keys(params)
+          .map((key) => `${snakeCase(key)} = :${key}`)
+          .join(" AND ")}`;
+      }
+      const generatedSql = `SELECT * FROM ${sqlOrTable} ${condition}`;
+      results = await this.execute(generatedSql, params);
+    }
+    return results?.[0];
   }
 
-  async getMany(sql, params): Promise<any[]> {
-    return this.execute(sql, params);
+  async getMany(
+    sqlOrTable: string,
+    params?: Record<string, unknown>
+  ): Promise<Record<string, unknown>[]> {
+    if (sqlOrTable.toLowerCase().startsWith("select")) {
+      return this.execute(sqlOrTable, params) as Promise<
+        Record<string, unknown>[]
+      >;
+    }
+
+    let condition = "";
+    if (params) {
+      condition = `WHERE ${Object.keys(params)
+        .map((key) => `${snakeCase(key)} = :${key}`)
+        .join(" AND ")}`;
+    }
+    const generatedSql = `SELECT * FROM ${sqlOrTable} ${condition}`;
+    return this.execute(generatedSql, params) as Promise<
+      Record<string, unknown>[]
+    >;
   }
 
-  async insert(table, columns) {
+  async insert(
+    table: string,
+    columns: Record<string, unknown>
+  ): Promise<unknown> {
     const keys = Object.keys(columns);
     const sql = `INSERT INTO 
       ${table} (${keys.map((a) => `${snakeCase(a)}`)}) 
@@ -76,7 +110,11 @@ class MySqlDb {
     return this.execute(sql, columns, QueryTypes.INSERT);
   }
 
-  async update(table, condition, columns) {
+  async update(
+    table: string,
+    condition: Record<string, unknown>,
+    columns: Record<string, unknown>
+  ): Promise<unknown> {
     const keys = Object.keys(columns);
     const conditionKeys = Object.keys(condition);
 
@@ -88,13 +126,28 @@ class MySqlDb {
     return this.execute(sql, { ...condition, ...columns }, QueryTypes.UPDATE);
   }
 
-  async delete(table, condition) {
+  async delete(
+    table: string,
+    condition: Record<string, unknown>
+  ): Promise<unknown> {
     const keys = Object.keys(condition);
     const sql = `DELETE FROM ${table} WHERE ${keys
       .map((k) => `${snakeCase(k)} = :${k}`)
       .join(" AND ")}`;
     return this.execute(sql, condition, QueryTypes.DELETE);
   }
+
+  private castRow(row) {
+    const keys = Object.keys(row);
+    const model = {};
+    keys.forEach(
+      (key) =>
+        (model[camelCase(key)] =
+          typeof row[key] === "object" ? this.castRow(row[key]) : row[key])
+    );
+    return model;
+  }
 }
 
 export default MySqlDb;
+export { QueryTypes };
