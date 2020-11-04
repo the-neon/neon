514cb0def893cd3264f9a306914fb75cf10cd58d
stojce <stojce@me.com>
2020-11-04 15:26:11 +0100

    [gql] scarar fixes

--- packages/gql/src/generator.test.ts
+++ packages/gql/src/generator.test.ts
@@ -2,7 +2,12 @@ import { createSourceFile, ScriptKind, ScriptTarget } from "typescript";
 import Delinter from "./generator/Delinter";
 
 describe("schema generator", () => {
-  const delinter = new Delinter();
+  let delinter: Delinter;
+
+  beforeEach(() => {
+    delinter = new Delinter();
+  });
+
   it.todo("provide schema");
 
   it("should ignore empty and non-existing folders", () => {
@@ -36,6 +41,58 @@ describe("schema generator", () => {
 
     expect(delinter.queries.length).toBe(1);
     expect(delinter.mutations.length).toBe(1);
+    expect(delinter._types.size).toBe(1);
+  });
+
+  it("should create correct GQL types for scalars", () => {
+    const sourceFileMutation = createSourceFile(
+      "nameAPI.ts",
+      `class Name {
+          @Post
+          createString(): Promise<string> {}
+
+          @Get
+          getString(): Promise<string[]> {}
+
+          @Post
+          createBool(): Promise<boolean> {}
+
+          @Get
+          getBool(): Promise<boolean[]> {}
+
+          @Post
+          createInt(): Promise<integer> {}
+
+          @Get
+          getInt(): Promise<integer[]> {}
+
+          @Post
+          createFloat(): Promise<float> {}
+
+          @Get
+          getFloat(): Promise<float[]> {}
+      }`,
+      ScriptTarget.ESNext,
+      true,
+      ScriptKind.TS
+    );
+
+    delinter.delint(sourceFileMutation);
+
+    const sourceFileModel = createSourceFile(
+      "userModel.ts",
+      `interface User {
+        name: string;
+      }`,
+      ScriptTarget.ESNext,
+      true,
+      ScriptKind.TS
+    );
+
+    delinter.delint(sourceFileModel);
+
+    expect(delinter.queries.length).toBe(4);
+    expect(delinter.mutations.length).toBe(4);
     expect(delinter._types.size).toBe(1);
   });
 });
